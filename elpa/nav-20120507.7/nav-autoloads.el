f7b12bba2f99f00af862b741d1a4665ee4dff2a6
chrish <childebrand@shfl.com>
2013-06-20 09:08:47 -0700

    Add nav

---
+++ elpa/nav-20120507.7/nav-autoloads.el
@@ -0,0 +1,34 @@
+;;; nav-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "ack" "ack.el" (20831 20821 0 0))
+;;; Generated autoloads from ack.el
+
+(defvar ack-history nil)
+
+;;;***
+
+;;;### (autoloads (nav) "nav" "nav.el" (20831 20821 0 0))
+;;; Generated autoloads from nav.el
+
+(autoload 'nav "nav" "\
+Opens Nav in a new window to the left of the current one.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("nav-pkg.el") (20831 20821 119549 0))
+
+;;;***
+
+(provide 'nav-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; nav-autoloads.el ends here
