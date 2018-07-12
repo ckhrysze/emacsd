99af51082be21b19f8b9f6d7b332604862519480
Chris Hildebrand <chris.hildebrand@versussystems.com>
2018-07-12 10:12:51 -0700

    Forgot the global part for line highlighting

--- elisp/modes.el
+++ elisp/modes.el
@@ -1,8 +1,8 @@
 (show-paren-mode t)
 (column-number-mode 1)
 (line-number-mode 1)
+(global-hl-line-mode 1)
 (global-linum-mode 1)
-(hl-line-mode 1)
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (scroll-bar-mode -1)
 
